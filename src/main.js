4e5a986f73f5bcccd4cec327bd42a0dddb87ed51
Dayvison Lima <limaare@outlook.com>
2023-02-16 14:44:04 -0300

    tentativa dos requisitos 4 e 5

--- src/main.js
+++ src/main.js
@@ -1,18 +1,42 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { createCustomElement, createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+const loading = createCustomElement('span', 'loading', 'loading...');
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const productsSection = document.querySelector('.products');
 
+const funtionLoading = () => productsSection.appendChild(loading);
+const removeLoading = () => productsSection.removeChild(loading);
+
+const errorMessage = 'Algum erro ocorreu, recarregue a página e tente novamente';
+const errorLoading = createCustomElement('span', 'error', errorMessage);
+const showError = () => productsSection.appendChild(errorLoading);
+
 const productList = async () => {
-  const products = await fetchProductsList('computador');
-  products.forEach((product) => {
-    const productElement = createProductElement(product);
-    productsSection.appendChild(productElement);
-  });
+  funtionLoading();
+  try {
+    const products = await fetchProductsList('computador');
+    products.forEach((product) => {
+      const productElement = createProductElement(product);
+      productsSection.appendChild(productElement);
+    });
+  } catch (error) {
+    showError();
+  } finally {
+    removeLoading();
+  }
 };
+
+// const productList = async () => {
+//   const products = await fetchProductsList('computador');
+//   products.forEach((product) => {
+//     const productElement = createProductElement(product);
+//     productsSection.appendChild(productElement);
+//   });
+// };
+
 
 window.onload = async () => {
   productList();
