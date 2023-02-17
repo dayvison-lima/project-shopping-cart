9586e593ecebfd54bf0eb156276efbc2ef6267fd
Dayvison Lima <limaare@outlook.com>
2023-02-17 13:01:21 -0300

    tentativa requisito 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,26 @@
-export const getAddress = () => {
+export const getAddress = async () => {
   // seu código aqui
+  const getCep = document.querySelector('.cep-input');
+  const cep = getCep.value;
+  const cartAdress = document.querySelector('.cart__address');
+
+  try {
+    const primeiroCep = `https://cep.awesomeapi.com.br/json/${cep}`;
+    const segundoCep = `https://brasilapi.com.br/api/cep/v2/${cep}`;
+    const promisses = await Promise.any([fetch(primeiroCep), fetch(segundoCep)]);
+    const data = await promisses.json();
+    return data;
+  } catch (error) {
+    cartAdress.innerHTML = 'CEP não encontrado';
+  }
 };
 
-export const searchCep = () => {
+export const searchCep = async () => {
   // seu código aqui
+  const cartAdress = document.querySelector('.cart__address');
+  const data = await getAddress();
+  const { address, district, city, state, street, neighborhood } = data;
+  const cepAdress = `${address}, ${district}, ${city}, ${state}`;
+  cartAdress.innerHTML = cepAdress;
+  // cartAdress.innerHTML = `${street}, ${neighborhood}, ${city}, ${state}`;
 };
