914e7373389189f72a529008f177948fc888f6df
Dayvison Lima <limaare@outlook.com>
2024-02-01 15:13:44 -0300

    completando o último requisito e editando o readme

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,28 +1,15 @@
-export const getAddress = async () => {
-  // seu código aqui
-  const getCep = document.querySelector('.cep-input').value;
-  // const cartAdress = document.querySelector('.cart__address');
-
-  const primeiroCep = `https://cep.awesomeapi.com.br/json/${getCep}`;
-  const segundoCep = `https://brasilapi.com.br/api/cep/v2/${getCep}`;
+export const getAddress = async (cep) => {
+  const primeiroCep = `https://cep.awesomeapi.com.br/json/${cep}`;
+  const segundoCep = `https://brasilapi.com.br/api/cep/v2/${cep}`;
   const promisses = await Promise.any([fetch(primeiroCep), fetch(segundoCep)]);
   const data = await promisses.json();
   return data;
 };
 
 export const searchCep = async () => {
-  // seu código aqui
   const cep = document.querySelector('.cep-input').value;
   const cartAdress = document.querySelector('.cart__address');
-  const data = getAddress();
-  // const data1 = await Promise.all(data);
-  // const { address, district, city, state } = data1;
-  // const tamanhoCep = 8;
-  // const erroMessage = 'CEP não encontrado';
-  // if (address && cep.length === tamanhoCep) {
-  //   cartAdress.innerHTML = `${address} - ${district} - ${city} - ${state}`;
-  // } else if (cep.length !== tamanhoCep || (!Object.keys(data1).includes('address'))) {
-  //   cartAdress.innerHTML = erroMessage;
+  const data = getAddress(cep);
   return Promise.resolve(data).then((data1) => {
     const { address, district, city, state } = data1;
     const tamanhoCep = 8;
